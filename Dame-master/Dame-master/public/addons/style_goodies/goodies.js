e41095037bf48a0b13b3342598385fb9615e9233
SIMPORE <nayimasimpore@gmail.com>
2020-04-16 14:19:18 +0200

    Mon projet

--- Dame-master/Dame-master/public/addons/style_goodies/goodies.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    $$('h2[id]').forEach((h) => {
-        h.append('a.int-ref  §  ').href = '#' + h.id;
-    });
-})();
--- Tron-game-master/Tron-game-master/Coordonnee.js
+++
@@ -1,7 +0,0 @@
-
-
-var neu = function(x, y){
-	return new Coordonnee(x, y);
-};
-
-module.exports.neu = Coordonnee;
